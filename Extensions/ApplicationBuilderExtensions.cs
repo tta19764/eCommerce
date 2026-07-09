10f629ab826a8371791f867df08fd5530f21f0de
tta19764 <tta19764@gmail.com>
2026-07-09 16:14:58 +0300

    Add ApplicationBuilderExtensions for shared middleware and migrations

---
+++ Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using eCommerce.SharedLibrary.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace eCommerce.SharedLibrary.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IHostBuilder UseSharedSerilog(this IHostBuilder host)
+    {
+        return host.UseSerilog((context, configuration) =>
+            configuration.ReadFrom.Configuration(context.Configuration));
+    }
+    
+    public static void ApplyMigrations<TContext>(this IApplicationBuilder app) where TContext : DbContext
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+
+        using var dbContext = scope.ServiceProvider.GetRequiredService<TContext>();
+
+        dbContext.Database.Migrate();
+    }
+    
+    public static IApplicationBuilder UseSharedMiddleware(this IApplicationBuilder app)
+    {
+        return app
+            .UseMiddleware<ExceptionHandlingMiddleware>()
+            .UseMiddleware<GatewayOnlyMiddleware>()
+            .UseMiddleware<RequestContextLoggingMiddleware>();
+    }
+}
